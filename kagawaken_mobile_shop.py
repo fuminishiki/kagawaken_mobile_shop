a5a6c1b7a11080a449b4b312699fd910911e0fd2
fuminishiki <136785393+fuminishiki@users.noreply.github.com>
2023-06-19 14:19:34 +0900

    Create kagawaken_mobile_shop.py

---
+++ kagawaken_mobile_shop.py
@@ -0,0 +1,252 @@
+import folium
+
+m = folium.Map(location=[34.3481671,134.0515907],zoom_start=10)
+m
+
+folium.Marker(location=[34.3519542,134.0049933],popup='ソフトバンク+ワイモバイル イオンモール高松店　　　　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.3096401,133.8150539],popup='ソフトバンク+ワイモバイル 宇多津店　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.2972481,133.9574396],popup='ソフトバンク+ワイモバイル 香川国分寺店　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.1205623,133.6796488],popup='ソフトバンク+ワイモバイル 観音寺店　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.3181795,133.8500959],popup='ソフトバンク+ワイモバイル 坂出店　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.2634732,134.134405],popup='ソフトバンク+ワイモバイル さぬき三木店　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.4868958,134.1924505],popup='ソフトバンク+ワイモバイル 小豆島店　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.2363266,133.7868781],popup='ソフトバンク+ワイモバイル 善通寺店　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.2881078,134.0272076],popup='ソフトバンク+ワイモバイル 高松一宮店　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.324088,134.0565581],popup='ソフトバンク+ワイモバイル 高松中央店　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.3446798,134.0141165],popup='ソフトバンク+ワイモバイル 高松西店　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.302451,134.0590432],popup='ソフトバンク+ワイモバイル 高松レインボーロード店　　　　　　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.278238,134.1023662],popup='ソフトバンク+ワイモバイル 十川店　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.2776618,133.7960901],popup='ソフトバンク+ワイモバイル 丸亀店　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.2576762,133.8131932],popup='ソフトバンク+ワイモバイル 丸亀中央店　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.2950505,133.8124196],popup='ソフトバンク+ワイモバイル 丸亀土器店　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.343985,134.0502988],popup='ソフトバンク+ワイモバイル 丸亀町商店街店　　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.3390635,134.1002475],popup='ソフトバンク+ワイモバイル 屋島店　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.3164443,134.0422779],popup='ソフトバンク+ワイモバイル ゆめタウン高松店　　　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.2731334,133.7845352],popup='ソフトバンク+ワイモバイル ゆめタウン丸亀店　　　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.1456063,133.6989670],popup='ソフトバンク+ワイモバイル ゆめタウン三豊店　　　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.347478,134.0669046],popup='ドコモ 朝日町店　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.2501,133.9311766],popup='ドコモ 綾川店　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.3100099,133.8146777],popup='ドコモ 宇多津店　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.308445,134.052564],popup='ドコモ 太田店　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.0963913,133.6582156],popup='ドコモ 大野原店　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.2762512,134.0986463],popup='ドコモ 川島店　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.1317986,133.6933149],popup='ドコモ 観音寺店　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.3244719,134.0717296],popup='ドコモ 木太店　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.2974736,133.9523418],popup='ドコモ 国分寺店　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.1868984,133.8388513],popup='ドコモ こんぴら店　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.3177389,133.8466318],popup='ドコモ 坂出店　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.2498612,134.3372668],popup='ドコモ 三本松店　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.4883782,134.1940767],popup='ドコモ 小豆島店　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.3213358,134.1759723],popup='ドコモ 志度店　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.2300752,133.7910211],popup='ドコモ 善通寺店　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.1830519,133.7198172],popup='ドコモ 高瀬店　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.2850047,134.0330634],popup='ドコモ 高松空港通り店　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.340922,133.9991892],popup='ドコモ 高松香西店　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.3252113,134.0446433],popup='ドコモ 高松店　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.3085908,134.0176088],popup='ドコモ 勅使店　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.2959347,133.8123232],popup='ドコモ 土器店　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.3459927,134.0496134],popup='ドコモ 兵庫町店　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.2813852,133.7969241],popup='ドコモ 丸亀店　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.2642165,134.1273505],popup='ドコモ 三木店　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.3395145,134.0980122],popup='ドコモ 屋島店　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.3163543,134.0422645],popup='ドコモ ゆめタウン高松店　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.2731974,133.7844862],popup='ドコモ ゆめタウン丸亀店　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.1458649,133.6988949],popup='ドコモ ゆめタウン三豊店　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.3106097,133.8139913],popup='au+UQモバイル 宇多津店　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.3344441,134.0919348],popup='au+UQモバイル 香川トヨタ自動車(株)屋島店　　　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.1328812,133.6933632],popup='au+UQモバイル 観音寺店　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.3339301,134.0788797],popup='au+UQモバイル 木太店　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.3442531,134.0165922],popup='au+UQモバイル 香西店　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.2976049,133.953244],popup='au+UQモバイル 国分寺店　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.3208557,134.1811622],popup='au+UQモバイル さぬき志度店　　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.2963054,134.0387512],popup='au+UQモバイル サンシャイン通り店　　　　　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.24933,134.3307757],popup='au+UQモバイル 三本松店　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.4762427,134.1798403],popup='au+UQモバイル 小豆島店　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.2393991,133.7938573],popup='au+UQモバイル 善通寺店　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.3447182,134.0500009],popup='au+UQモバイル 高松丸亀町店　　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.319844,134.0631764],popup='au+UQモバイル 高松レインボーロード店　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.2768619,134.1025802],popup='au+UQモバイル 十川店　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.3398692,134.080559],popup='au+UQモバイル ネッツトヨタ香川(株)本店　　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.3416569,134.0031676],popup='au+UQモバイル ネッツトヨタ高松(株)本社店　　　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.2930332,134.0119252],popup='au+UQモバイル ハローズ円座店　　　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.2956631,133.8124606],popup='au+UQモバイル 丸亀土器店　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.2639262,134.1372823],popup='au+UQモバイル 三木店　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.3394827,134.1002181],popup='au+UQモバイル 屋島店　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.3165343,134.0422913],popup='au+UQモバイル ゆめタウン高松店　　　　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.2731329,133.7856675],popup='au+UQモバイル ゆめタウン丸亀店　　　　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.1452911,133.6983816],popup='ワイモバイル ゆめタウン三豊店　　　　　　　　　　　　　　',icon=folium.Icon(color="lightred")).add_to(m)
+m
+
+folium.Marker(location=[34.3161140,134.0422987],popup='UQモバイル ゆめタウン高松店　　　　　　　　　　　　　',icon=folium.Icon(color="lightblue")).add_to(m)
+m
+
+folium.Marker(location=[34.2734162,133.7859035],popup='UQモバイル ゆめタウン丸亀店　　　　　　　　　　　　　',icon=folium.Icon(color="lightblue")).add_to(m)
+m
+
+folium.Marker(location=[34.3161140,134.0424199],popup='楽天モバイル ゆめタウン高松店　　　　　　　　　　　　　　',icon=folium.Icon(color="pink")).add_to(m)
+m
+
+folium.Marker(location=[34.3518900,134.0052059],popup='イオンモバイル 高松店　　　　　　　　　　',icon=folium.Icon(color="purple")).add_to(m)
+m
+
+folium.Marker(location=[34.3456152,134.0715872],popup='イオンモバイル 高松東店　　　　　　　　　　　',icon=folium.Icon(color="purple")).add_to(m)
+m
+
+folium.Marker(location=[34.3082774,133.8097684],popup='ピカラモバイル イオンタウン宇多津店　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="blue")).add_to(m)
+m
+
+folium.Marker(location=[34.2518059,133.9309047],popup='ピカラモバイル イオンモール綾川店　　　　　　　　　　　　　　　　',icon=folium.Icon(color="blue")).add_to(m)
+m
+
+folium.Marker(location=[34.3518792,134.0045679],popup='ピカラモバイル イオンモール高松店　　　　　　　　　　　　　　　　',icon=folium.Icon(color="blue")).add_to(m)
+m
+
+folium.Marker(location=[34.3428103,134.0497217],popup='ピカラモバイル 高松丸亀町店　　　　　　　　　　　　　',icon=folium.Icon(color="blue")).add_to(m)
+m
+
+folium.Marker(location=[34.3481671,134.0515907],popup='マーキュリー 香川営業所　　　　　　　　　　　',icon=folium.Icon(color="white")).add_to(m)
+m
+
+m.save('kagawaken_mobile_shop.html')
